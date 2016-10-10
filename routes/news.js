56fb9532d642809724687ac4657b6aab669799f7
lipingwang <lipingwang@gstarcloud.com>
2016-10-10 13:10:03 +0800

    [add] admin post

--- routes/news.js
+++ routes/news.js
@@ -7,20 +7,20 @@ mongoose.connect('mongodb://localhost/news')
 
 /* GET news listing. */
 router.get('/', function(req, res, next) {
-	News.fetch(function(err, news) {
-		if(err){
-			console.log(err);
-		}
+    News.fetch(function(err, news) {
+        if(err){
+            console.log(err);
+        }
 
-		res.render('list', {
-			title: 'News List',
-			news: news
-		})
-	})
+        res.render('list', {
+            title: 'News List',
+            news: news
+        })
+    })
 });
 
 router.post('/', function(req, res, next){
-	res.send('post sucess.');
+    res.send('post sucess.');
 });
 
 /* admin news */
@@ -28,27 +28,68 @@ router.post('/', function(req, res, next){
 router.get('/admin', function(req, res, next) {
   // res.send('welcome to news.');
   res.render('admin', {
-  	title: 'news 录入页',
-	news: {
-		type_name: '',
-		question: '',
-		answer_pic: '',
-		answer_info: '',
-		answer_solution: ''
-	}
+    title: 'news 录入页',
+    news: {
+        type_name: '',
+        question: '',
+        answer_pic: '',
+        answer_info: '',
+        answer_solution: ''
+    }
   });
 });
 
+router.post('/admin', function(req, res) {
+  var id = req.body.news._id
+    var newsObj = req.body.news
+
+    var _news
+
+    if(id !=='undefined'){
+        News.findById(id,function(err,news){
+            if(err){
+                console.log(err)
+            }
+
+            _news = _.extend(news,newsObj)
+            _news.save(function(err,news){
+                if(err){
+                    console.log(err)
+                }
+
+                res.redirect('/news/'+_news.id)
+            })
+
+        })
+    }else{
+        _news = new News({
+            type_name:newsObj.type_name,
+            question:newsObj.question,
+            answer_pic:newsObj.answer_pic,
+            answer_info:newsObj.answer_info,
+            answer_solution:newsObj.answer_solution,
+        })
+
+        _news.save(function(err,news){
+                if(err){
+                    console.log(err)
+                }
+
+                res.redirect('/news/'+_news.id)
+            })
+    }
+})
+
 /* GET news detail. */
 router.get('/:id', function(req, res, next) {
-	var id = req.params.id
+    var id = req.params.id
 
-	News.findById(id,function(err,news){
-		res.render('detail',{
-				title:'new detail',
-				news:news
-		})
-	})
+    News.findById(id,function(err,news){
+        res.render('detail',{
+                title:'new detail',
+                news:news
+        })
+    })
 });
 
 module.exports = router;
