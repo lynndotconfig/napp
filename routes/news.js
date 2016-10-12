3b36b9d4fca54b4e39e40ec847b43cc98ba4da1d
lipingwang <lipingwang@gstarcloud.com>
2016-10-12 09:42:53 +0800

    [add] upload file.

--- routes/news.js
+++ routes/news.js
@@ -45,7 +45,7 @@ router.get('/admin', function(req, res) {
 router.post('/create', function(req, res) {
     var form = new multiparty.Form({uploadDir: '.public/images/'});
     form.parse(req, function (err, fields, files) {
-        var filesTmp = JSON.stringify(files.null, 2);
+        var filesTmp = JSON.stringify(files, null, 2);
         if (err) {
             console.log('parse error: ' + err);}
         var inputFile = files.inputFile[0];
