0a7ea490656a6c4c8e894aa78ba226bc210ff9e1
lipingwang <lipingwang@gstarcloud.com>
2016-10-13 10:00:59 +0800

    [fix] ueditor routes

--- routes/ueditor.js
+++ routes/ueditor.js
@@ -4,12 +4,7 @@ var router = express.Router();
 
 var ueditor = require('ueditor')
 
-/* GET home page. */
-router.post('/ue', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/ue', ueditor(path.join(__dirname, 'public'), function(req, res){
+router.use('/ue', ueditor(path.join(__dirname, 'public'), function(req, res){
 	if (req.query.action === 'uploadimage'){
 		var foo = req.ueditor;
 		var imgmane = req.ueditor.filename;
